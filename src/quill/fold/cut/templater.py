61e0d3f4cbef8fd7b3157d4bc747b63d70c5574c
Louis Maddox <louismmx@gmail.com>
2021-08-26 12:18:03 +0100

    Complete site build in a single template pass

--- src/quill/fold/cut/templater.py
+++ src/quill/fold/cut/templater.py
@@ -4,12 +4,15 @@ from datetime import datetime as dt
 from staticjinja import Site
 from functools import partial
 import markdown
+import frontmatter
+import dateparser
 
 __all__ = ["standup"]
 
 markdowner = markdown.Markdown(output_format="html5")
 
-def standup(domains_list=None, verbose=True):
+
+def standup(domains_list=None, verbose=True, out_dirname="site", post_dirname="posts"):
     domains_list = domains_list or [*ns]
     for domain in domains_list:
         ns_p = ns_path / domain
@@ -17,71 +20,102 @@ def standup(domains_list=None, verbose=True):
             raise ValueError(f"{ns_p} not found")
         template_dir = ns_p / "templates"
         if template_dir.exists():
-            site_dir = ns_p / "site"
-            article_dir = ns_p / "posts"
+            site_dir = ns_p / out_dirname
+            post_dir = ns_p / post_dirname
             extra_ctxs = []
-            if article_dir.exists():
-                for a in article_dir.iterdir():
+            if post_dir.exists():
+                for a in post_dir.iterdir():
                     print(a)
+                post_leaf_dir = Path(post_dirname)
                 post_ctxs = [
-                    (a.name, article)
-                    for a in article_dir.iterdir()
+                    (str(post_leaf_dir / a.name), article)
+                    for a in post_dir.iterdir()
                 ]
-                cut_templates(article_dir, site_dir, contexts=post_ctxs)
-                extra_ctxs.extend([
-                    ("index.html", partial(indexed_articles, article_dir=article_dir))
-                ])
-            cut_templates(template_dir, site_dir, contexts=extra_ctxs)
+                extra_ctxs.extend(post_ctxs)
+                extra_ctxs.extend(
+                    [("index.html", partial(indexed_articles, dir_path=post_dir))]
+                )
+            cut_templates(template_dir=template_dir, out_dir=site_dir, contexts=extra_ctxs)
             print(f"Built {template_dir}")
+
 
 def fmt_mtime(path_to_file):
     mtime = path_to_file.stat().st_mtime
     date = dt.fromtimestamp(mtime)
     return date.strftime("%Y-%m-%dT%H:%M:%SZ")
 
+
 def base(template):
+    print(f"Rendering {template} (base)")
     template_path = Path(template.filename)
     return {
         "template_date": fmt_mtime(template_path),
     }
 
+
 def index(template):
+    print(f"Rendering {template} (index)")
     return {}
 
-def indexed_articles(template, article_dir):
+
+def indexed_articles(template, dir_path):
+    "Sort articles by date"
     return {
-        "articles": [
-            article(a, is_path=True)
-            for a in article_dir.iterdir()
-            if not a.is_dir() # not sub-directory
-            if not a.name.startswith("_") # not partial template
-        ]
+        "articles": sorted(
+            [
+                article(a, is_path=True)
+                for a in dir_path.iterdir()
+                if not a.is_dir()  # not sub-directory
+                if not a.name.startswith("_")  # not partial template
+            ],
+            key=lambda d: d["date"]
+        )
     }
+
 
 def article(template, is_path=False):
+    print(f"Rendering {template} (article)")
     template_path = template if is_path else Path(template.filename)
-    return {
-        "url": template_path.stem, "title": template_path.stem,
-        "mtime": fmt_mtime(template_path), "short_desc": "foo bar baz",
-    }
+    if template_path.suffix != ".md":
+        raise ValueError("Metadata is not supported for non-markdown articles")
+    md_content = frontmatter.load(template_path)
+    metadata = md_content.metadata
+    required_keys = {"title", "desc", "date"}
+    if not all(k in metadata for k in required_keys):
+        raise ValueError(f"{template=} missing one or more of {required_keys=}")
+    return {"url": template_path.stem, "mtime": fmt_mtime(template_path), **metadata}
+
 
 def md_context(template):
-    markdown_content = Path(template.filename).read_text()
-    return {"post_content_html": markdowner.convert(markdown_content)}
+    print(f"Rendering {template} (md context)")
+    # markdown_content = Path(template.filename).read_text()
+    md_content = frontmatter.load(template.filename)
+    return {"post_content_html": markdowner.convert(md_content.content)}
+
 
 def render_md(site, template, **kwargs):
+    print(f"Rendering {template} (md)")
     # i.e. posts/post1.md -> build/posts/post1.html
     out = site.outpath / Path(template.name).with_suffix(".html")
     # Compile and stream the result
-    site.get_template("layouts/_post.html").stream(**kwargs).dump(str(out), encoding="utf-8")
+    site.get_template("templates/layouts/_post.html").stream(**kwargs).dump(
+        str(out), encoding="utf-8"
+    )
+
 
 def cut_templates(template_dir, out_dir, contexts=None, mergecontexts=True):
-    default_ctxs = [(r".*\.(html|md)$", base), ("index.html", index), (r".*\.md", md_context)]
+    default_ctxs = [
+        (r".*\.(html|md)$", base),
+        ("index.html", index),
+        (r".*\.md", md_context),
+    ]
     custom_contexts = contexts or []
     contexts = default_ctxs + custom_contexts
     site = Site.make_site(
-        contexts=contexts, mergecontexts=mergecontexts,
-        searchpath=template_dir, outpath=out_dir,
+        contexts=contexts,
+        mergecontexts=mergecontexts,
+        searchpath=template_dir,
+        outpath=out_dir,
         rules=[(r".*\.md", render_md)],
     )
     site.render()
